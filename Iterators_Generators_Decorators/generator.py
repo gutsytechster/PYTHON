49a034bbea3549fd4dcbd3515ebb7c1a4fa99e2d
gutsytechster <prashantsharma161198@gmail.com>
2018-04-19 00:40:42 +0530

    Programs in 'Iterators, generators and decorators' lesson

---
+++ Iterators_Generators_Decorators/generator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def counter_generator(low, high):
+    while low <= high:
+        yield low
+        low += 1
+
+for i in counter_generator(5, 10):
+    print(i, end=' ')
+
