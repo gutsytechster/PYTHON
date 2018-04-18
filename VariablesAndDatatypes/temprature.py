138ed8eaf819af580b8df53ea1fef1d86e937b31
gutsytechster <prashantsharma161198@gmail.com>
2018-04-18 23:58:05 +0530

    Programs in 'Variables And Datatypes' lesson

---
+++ VariablesAndDatatypes/temprature.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+fahrenheit = 0.0
+print("Fahrenheit Celsius")
+while fahrenheit <= 250:
+    celsius = (fahrenheit - 32.0) / 1.8 # Here we calculate the Celsius value
+    print("%5.1f %7.2f" % (fahrenheit, celsius))
+    fahrenheit += 25
